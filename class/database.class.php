1a4dbb6fcf9c5db0a69745b2800995e15d7a7206
Sven Brettschneider <brettschneider.sven@gmail.com>
2014-03-06 00:44:07 +0100

    basis opgezet menu en design

---
+++ class/database.class.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * This is my first database class!
+ *
+ * @author sven
+ */
+class database {
+    private $my_sqli_connect;
+    private $result;
+    
+    function __construct($schema) {
+        $mysqli = new mysqli("mysql03.totaalholding.nl", "seventho_school", "avans123", $schema);
+        
+        /* check connection */
+        if (mysqli_connect_errno()) {
+            printf("Connect failed: %s\n", mysqli_connect_error());
+            exit();
+        }
+ 
+        $this->my_sqli_connect = $mysqli;
+    }
+    
+    public function do_sql($query){
+        $this->result = $this->my_sqli_connect->query($query);
+    }
+    
+    public function get_array_from_query($query){
+        $this->result = $this->my_sqli_connect->query($query);
+
+        if( ! $this->result )
+        {
+            echo $this->my_sqli_connect->connect_error;
+            exit;
+        }
+
+        $aResult = array();
+
+        while( $r = $this->result->fetch_assoc() )
+        {
+            $aResult[] = $r;
+        }
+
+        print_r($aResult);
+    }
+    
+    public function check_login($username, $password){
+        $username = $this->my_sqli_connect->real_escape_string($username);
+        $password = $this->my_sqli_connect->real_escape_string($password);
+        
+        $inlog_check_query = "SELECT count(customer_username) as accepted FROM Account WHERE customer_username=? AND customer_pass=?";
+        
+        $stmt = $this->my_sqli_connect->stmt_init();
+        
+        if($stmt->prepare($inlog_check_query)){
+            $stmt->bind_param("ss", $username, $password);
+            $stmt->execute();
+            
+            $result = $stmt->get_result();
+            
+            return $result->fetch_object()->accepted;
+            
+        }else{
+            echo "It was not possible to prepare check_login statement";
+        }
+        
+                
+    }
+    
+    public function get_record(){
+        if($this->result){
+            return $this->result->fetch_object();
+        }else{
+            return false;
+        }
+    }
+    
+    public function __destruct() {
+        $this->result->close();
+        $this->my_sqli_connect->close();
+    }
+}
---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="css/std.css" rel="stylesheet" type="text/css"/>
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.0/jquery.min.js">
+            //stuff for the menu (jquery)
+        </script>
+    </head>
+    <body>
+        <div class="main_container">
+            <header class="maindiv">header</header>
+            <menu>
+                menu
+                <?php include 'inc/menu.inc.php'; ?>
+            </menu>
+            
+            <article class="product_container">Artikel</article>
+        </div>
+        
+        <footer></footer>
+    </body>
+</html>
