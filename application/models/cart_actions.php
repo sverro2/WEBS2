cfd6b9d5a0c2ce7733ff6f822d07c4d31f44fbec
Sven Brettschneider <brettschneider.sven@gmail.com>
2014-04-02 08:30:09 +0200

    sessies kunnen nu niet meer kappot worden gemaakt door een verkeerde id mee te geven

--- application/models/cart_actions.php
+++ application/models/cart_actions.php
@@ -4,6 +4,9 @@ session_start();
 ini_set('display_errors', 1);
 
 foreach (array_keys($_POST) as $item){
-    $_SESSION['shopping_cart']->amount_cart($item, $_POST[$item]);
-    echo $item;
+    $value = filter_var($_POST[$item], FILTER_VALIDATE_INT);
+    
+    if($value){
+        $_SESSION['shopping_cart']->amount_cart($item, $_POST[$item]);
+    }
 }
